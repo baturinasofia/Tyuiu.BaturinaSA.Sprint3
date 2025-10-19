856ef535dc110cafdc88255baa2b793fbfe3bd31
baturinasofia <sofi1608@bk.ru>
2025-10-19 17:38:43 +0500

    Task7

---
+++ Tyuiu.BaturinaSA.Sprint3.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.BaturinaSA.Sprint3.Task7.V11.Lib
+{
+    public class DataService : ISprint3Task7V11
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (((2 - 2 * x) == 0))
+                {
+                    valueArray[count] = 0.00;
+                }
+                else
+                {
+                    y = Math.Round(Math.Cos(x) + (Math.Sin(x) / (2 - 2 * x)) - 4 * x, 2);
+                    valueArray[count] = y;
+                }
+                count++;
+
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.BaturinaSA.Sprint3.Task7.V11/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.BaturinaSA.Sprint3.Task7.V11.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint3.Task7.V11;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнила: Батурина С. А. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнила: Батурина Софья Алексеевна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая выводит таблицу значений функции:           *");
+        Console.WriteLine("* F(x) = Cos(х) + (Sin(х)/(2-2х))-4х                                        *");
+        Console.WriteLine("* (произвести табулирование) f(x) на заданном диапазоне [-5;5]            *");
+        Console.WriteLine("* с шагом 1. Произвести проверку деления на ноль.                         *");
+        Console.WriteLine("* При делении на ноль вернуть значение 0.                                 *");
+        Console.WriteLine("* Значение округлить до двух знаков после запятой.                        *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        int len = ds.GetMassFunction(startValue, stopValue).Length;
+        double[] valueArray;
+        valueArray = new double[len];
+        valueArray = ds.GetMassFunction(startValue, stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("+----------+----------+");
+        Console.WriteLine("|    X     |   f(x)   |");
+        Console.WriteLine("+----------+----------+");
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine("|{0,5:d}     |  {1,5:f2}   |", startValue, valueArray[i]);
+            startValue++;
+        }
+        Console.WriteLine("+----------+----------+");
+        Console.ReadKey();
+    }
+}
